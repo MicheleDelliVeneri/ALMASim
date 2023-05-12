2b63b04b2e00c46c5560a0fdc5f364dee80c47dc
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2023-05-12 15:37:36 +0200

    ok

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='ALMASim',
     version='1.1.0',
-    packages=find_namespace_packages(),
+    packages=['illustris_python'],
+    package_dir={'illustris_python': 'illustris_python'},
     install_requires=["numpy", "h5py", "six", 
                       "astropy==5.1.1", "scipy", "matplotlib", "pandas", "tqdm",
                       "h5py", "hdecompose", "spectral_cube", "astromartini", 
                       "dask", "dask[distributed]", "natsort", "distributed", "casatools==6.5.5.21", 
-                      "casatasks==6.5.5.21", "casadata==2023.4.10", 
+                      "casatasks==6.5.5.21", "casadata==2023.4.10",
                       
                       ],
     tests_require=["nose","coverage"],
