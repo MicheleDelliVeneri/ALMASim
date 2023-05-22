1910e487912c36b57855f48938dbf5f385b9acd5
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2023-05-22 17:12:07 +0200

    ok

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_namespace_packages(),
     install_requires=["numpy", "h5py", "six", 
                       "astropy==5.1.1", "scipy", "matplotlib", "pandas", "tqdm",
-                      "h5py", "hdecompose", "spectral_cube", "astromartini", 
+                      "h5py", "hdecompose", "spectral-cube", "astromartini", 
                       "dask", "dask[distributed]", "natsort", "distributed", "casatools==6.5.5.21", 
                       "casatasks==6.5.5.21", "casadata==2023.4.10",
                       
