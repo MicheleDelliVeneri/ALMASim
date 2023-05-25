18636f3d29e53092a0d3e5a543ebf53a54dbd29d
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2023-05-25 15:57:31 +0200

    2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Michele Delli Veneri"
 author = "Michele Delli Veneri"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1"
+release = "2.2"
 
 
 # -- General configuration ---------------------------------------------------
