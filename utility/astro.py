a4c53ca36fc1fc8a190394c30184efa84b3ff99f
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2024-05-15 22:01:57 +0200

    fix:typo

--- utility/astro.py
+++ utility/astro.py
@@ -1035,7 +1035,7 @@ def get_line_name_from_rest_frequency(master_path, rest_frequencies):
 def sample_given_redshift(metadata, n, rest_frequency, extended, zmax=None):
     pd.options.mode.chained_assignment = None
     print(np.sort(np.array(rest_frequency)))
-    print(f'Source Frequency: {source_frequency}')
+    
     if isinstance(rest_frequency, np.ndarray):
         rest_frequency = np.sort(np.array(rest_frequency))[0]
     print(f'Filtering metadata based on rest frequency of selected lines: {rest_frequency}')
@@ -1044,6 +1044,7 @@ def sample_given_redshift(metadata, n, rest_frequency, extended, zmax=None):
     metadata = metadata[metadata['Freq'] >= rest_frequency]
     print(f'Remaining metadata: {len(metadata)}')
     freqs = metadata['Freq'].values
+    print(f'Source Frequency: {freqs}')
     redshifts = [compute_redshift(rest_frequency * U.GHz, source_freq * U.GHz) for source_freq in freqs]
     metadata.loc[:, 'redshift'] = redshifts
 
