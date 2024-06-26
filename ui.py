8ff0a52ae958de05dd7e7702956ea8fa0ae2ca97
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2024-06-26 17:53:12 +0200

    fix: also removing commands from terminal

--- ui.py
+++ ui.py
@@ -1968,20 +1968,18 @@ class ALMASimulator(QMainWindow):
             # Call initiate_parallel_simulation_remote with window instance
             python -c "import sys; import os; import ui; from PyQt6.QtWidgets import QApplication; app = QApplication(sys.argv); ui.ALMASimulator.settings_file = '{settings_path}'; window=ui.ALMASimulator(); ui.ALMASimulator.initiate_parallel_simulation_remote(window); sys.exit(app.exec())"
         """
-        exclude_pattern = re.compile(r'\[\d+m|\[\?\d+h|\[\w+;\d+m|\[\d+;\d+m|\x1B\[[0-?]*[ -/]*[@-~]')
+        exclude_pattern = re.compile(r"""
+            # Pattern 1: Specific command lines
+            (^cd /home/astro/ALMASim$)
+            |(source /home/astro/almasim_env/bin/activate$)
+            |(export QT_QPA_PLATFORM=offscreen$)
+            |(python -c "import sys.*initiate_parallel_simulation_remote\(window\); sys.exit\(app.exec\(\)"\s*$)
+            # Pattern 2: ANSI escape codes
+            |(\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~]))  
+        """, re.VERBOSE) 
         paramiko_client = paramiko.SSHClient()
         paramiko_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         paramiko_client.connect(self.remote_address_entry.text(), username=self.remote_user_entry.text(), pkey=key)
-        #stdin, stdout, stderr = paramiko_client.exec_command(dask_commands)
-        #while True:
-        #    line = stdout.readline()
-        #    if not line:
-        #        break
-        #    self.terminal.add_log(line.strip())
-    
-        #err = stderr.read().decode()
-        #if err:
-        #    self.terminal.add_log(err)
         channel = paramiko_client.invoke_shell()
         # Continuously read and display output
         def read_output():
