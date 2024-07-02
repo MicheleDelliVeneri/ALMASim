91f29dd17ab245bbf15e0557572e34f55baa6dde
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2024-07-02 12:34:57 +0200

    fix: partial refactoring and working on publish

---
+++ almasim/__init__.py
@@ -0,0 +1,10 @@
+from .__version__ import __version__
+
+
+__doc__ = """
+    ALMASim an elegant simulator for the ALMA Observatory for a more civilized age.
+
+    ALMASim is a package for simulating the ALMA Observatory. 
+    It is a graphical user interface that allows users to simulate the ALMA Observatory and its observations. 
+    It is designed to be user-friendly and easy to use.
+"""
