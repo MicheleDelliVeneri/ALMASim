9f5f63d6b426227d5d3469cce0bf0378a094d509
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2024-07-03 15:50:31 +0200

    test: added foulthandler

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -3,7 +3,8 @@ from pytestqt.qtbot import QtBot
 from PyQt6.QtWidgets import QApplication
 import almasim.ui as ui
 import sys
-
+import faulthandler
+faulthandler.enable()
 
 @pytest.fixture(scope="session")
 def app():
