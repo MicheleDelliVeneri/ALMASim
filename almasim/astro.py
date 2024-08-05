fix: redsfhit computation and snr

almasim/astro.py
@@ -29,7 +29,7 @@ def compute_redshift(rest_frequency, observed_frequency):
         raise ValueError("Observed frequency must be lower than the rest frequency.")
 
     # Compute redshift
-    redshift = (rest_frequency.value - observed_frequency.value) / rest_frequency.value
+    redshift = (rest_frequency.value - observed_frequency.value) / observed_frequency.value
     return redshift
 
 
@@ -307,12 +307,3 @@ def write_sim_parameters(
             f.write("TNG Snapshot ID: {}\n".format(snapshot))
             f.write("TNG Subhalo ID: {}\n".format(subhalo))
         f.close()
-
-
-# def get_image_from_ssd(ra, dec, fov):
-#    DEF_ACCESS_URL = "https://datalab.noirlab.edu/sia/sdss_dr9"
-#    svc_sdss_dr9 = sia.SIAService(DEF_ACCESS_URL)
-#    ac.whoAmI()
-#    imgTable = svc_sdss_dr9.search(
-#        (ra, dec), (fov / np.cos(dec * np.pi / 180), fov), verbosity=2
-#    ).to_table()

almasim/ui.py
@@ -4137,6 +4137,7 @@ class ALMASimulator(QMainWindow):
             print("Observing with ALMA")
         else:
             self.terminal.add_log("Observing with ALMA")
+        min_line_flux = np.min(line_fluxes)
         interferometer = uin.Interferometer(
             inx,
             model,
@@ -4148,7 +4149,7 @@ class ALMASimulator(QMainWindow):
             band_range,
             fov,
             antenna_array,
-            np.min(line_fluxes) / snr,
+            min_line_flux / snr,
             snr,
             int_time.value * second2hour,
             obs_date,